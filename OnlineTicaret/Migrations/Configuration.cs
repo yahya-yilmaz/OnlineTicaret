a4d6370aaa7487b20b1fb8f1029e52de61794c12
yahya-yilmaz <yiilmazyahya@gmail.com>
2023-03-09 10:49:02 +0300

    Add project files.

---
+++ OnlineTicaret/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineTicaret.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineTicaret.Models.Sınıflar.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(OnlineTicaret.Models.Sınıflar.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
